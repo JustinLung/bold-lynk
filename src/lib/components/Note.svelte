a5f3bd15d58de92295bae10a4a5a7dc2fe8fa89f
JustinLung <justinlung77@gmail.com>
2022-11-08 10:24:32 +0100

    export prop from note component

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import BoxIcon from './icons/BoxIcon.svelte';
+	export let noteText: string;
 </script>
 
 <div class="bg-color-darkgrey flex items-center py-2 px-6  gap-6 w-fit rounded-lg">
 	<BoxIcon />
-	<p>what they say</p>
+	<p>{noteText}</p>
 </div>
